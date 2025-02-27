2035968f0ce7adb32babee992c2fa0b8a9a73617
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-02-27 13:54:32 -0300

    Feat: teste dos bons

---
+++ qrcode/android/app/src/main/kotlin/br/com/aynova/compras_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.aynova.compras_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- qrcode/android/build.gradle.kts
+++ qrcode/android/build.gradle.kts
@@ -1,50 +1,18 @@
-import java.util.Properties
-
-plugins {
-    id("com.android.library") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "1.9.22"
-}
-
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.reader(Charsets.UTF_8).use { reader ->
-        localProperties.load(reader)
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-android {
-    namespace = "com.example.qrcode"
-    compileSdk = 34
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 34
-    }
-
-    buildFeatures {
-        buildConfig = true
-    }
-
-    lint {
-        disable += "InvalidPackage"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
-    implementation("com.google.zxing:core:3.5.1") // ZXing para leitura de QR Code
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0") // Biblioteca para QR Code
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
